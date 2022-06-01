86893bb408037b5027f17d663279bf31caf8d4b4
Joshua Rei <xxstubzxx@gmail.com>
2022-05-31 21:01:02 -0400

    Styling Updates/Static Resources
    Adds some data updates as well as additional styling as well as static resource setup for logo and css etc.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 /** Server Modules **/
 import { opine } from 'https://deno.land/x/opine@2.2.0/mod.ts';
+import staticFiles from "https://deno.land/x/static_files@1.1.6/mod.ts";
 
 /** Custom Modules **/
 import { HomeShow } from './controllers/Home.js';
@@ -9,6 +10,8 @@ import { GetSwapiRoot } from './controllers/Swapi.js';
 
 /** Start Our Server **/
 const server = opine();
+
+server.use(staticFiles('public'));
 
 server.get('/', async (request, response) => {
     response.body = await HomeShow();
--- views/layouts/main.js
+++ views/layouts/main.js
@@ -1,14 +1,18 @@
-'use strict'
+import loadJsonFile from "https://deno.land/x/load_json_file@v1.0.0/mod.ts";
 
 export default async (data) => {
+    const LayoutData = await loadJsonFile("views/layouts/main.json");
+    const { Language, Title } = LayoutData;
+
     return `
     <!DOCTYPE html>
-        <html lang="${data.Language}">
+        <html lang="${Language}">
             <head>
+                <link rel="icon" href="logo.jpg">
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                <title>${data.PageTitle}</title>
+                <title>${Title}</title>
             </head>
             ${data.Navigation}
             <body>
