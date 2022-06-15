a1cd704132e48fefbdc5daaa92a805211dc8c94d
Joshua Rei <xxstubzxx@gmail.com>
2022-06-15 00:03:53 -0400

    WIP Three
    PersonnelList, Add additional Personnel JSON entries. Linking to Masters page if available. Some skill levels are being driven off of JSON data. Expanded master to have display value and data value for master lookups. rename personnel from personal across the code. base keep JSON consistent will most likely build out JSON into it's own DENO run API.

--- templates.js
+++ templates.js
@@ -3,8 +3,9 @@ import MainLayout from "./views/layouts/Main/main.js";
 import HomeTemplate from "./views/pages/Home/home.js";
 import AboutTemplate from "./views/pages/About/about.js";
 import PersonalTemplate from "./views/pages/Personal/personal.js";
+import PersonalListTemplate from "./views/pages/Personal/personal.js";
 
 /** Partials **/
 import Navigation from "./views/partials/Navigation/navigation.js";
 
-export { AboutTemplate, HomeTemplate, PersonalTemplate, MainLayout, Navigation };
+export { AboutTemplate, HomeTemplate, PersonalListTemplate, PersonalTemplate, MainLayout, Navigation };
--- views/layouts/Main/main.js
+++ views/layouts/Main/main.js
@@ -3,7 +3,7 @@ import { BaseStyles } from "../../../styles.js";
 
 export default async (data) => {
   const LayoutData = await loadJsonFile("views/layouts/Main/main.json");
-  const { Language, Title } = LayoutData;
+  const { Language, Title, Body } = LayoutData;
 
   return `
     <!DOCTYPE html>
@@ -18,8 +18,7 @@ export default async (data) => {
                     ${BaseStyles()}
                 </style>
             </head>
-            ${data.Navigation}
-            <body>
+            <body class="${Body.Classes}">
                 ${data.Body}
                 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
                 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
